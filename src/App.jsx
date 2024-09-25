58c2900995eaf15c2991929209f1134a91a4e86d
Vitalii <vitalii@Laptop-Vitalii.local>
2024-09-25 19:29:09 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { useEffect } from 'react';
+import './App.css'
+const tg = window.Telegram.WebApp;
+function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+ 
+  const onClose = () => {
+     tg.close()
+  }
+ 
+
+  return (
+    <>
+      <div className='App'>
+        <span>Hello !!!</span> 
+        <button onClick = {onClose}>Закрыть</button>
+      </div>
+    </>
+  )
+}
+
+export default App
